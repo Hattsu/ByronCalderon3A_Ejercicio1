2bde282b5f62ba8524de3d74ef549c129b151db0
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-19 12:53:10 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace ByronCalderon3A_Ejercicio1
     {
         static void Main(string[] args)
         {
-            Num_mayor num_mayor = new Num_mayor (10.5, 5.4d);
+            Num_mayor num_mayor = new Num_mayor (10.5, 5.4);
             num_mayor.Elegir_num(); 
         }
     }
